77423b2952eb248a9b9f1be3bddf70557dc83d80
Jon Best <143686006+Jon-Best-SWE@users.noreply.github.com>
2026-06-05 14:55:01 -0600

    Update populate.py
    Final update for lint.

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -18,7 +18,8 @@ def populate_database():
 
     for data in car_make_data:
         car_make_instances.append(
-            CarMake.objects.create(name=data["name"], description=data["description"])
+            CarMake.objects.create(name=data["name"], 
+            description=data["description"])
         )
 
     car_model_data = [
@@ -58,9 +59,24 @@ def populate_database():
             "year": 2023,
             "car_make": car_make_instances[1],
         },
-        {"name": "A4", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
-        {"name": "A5", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
-        {"name": "A6", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
+        {
+            "name": "A4",
+            "type": "SUV",
+            "year": 2023,
+            "car_make": car_make_instances[2],
+        },
+        {
+            "name": "A5",
+            "type": "SUV",
+            "year": 2023,
+            "car_make": car_make_instances[2],
+        },
+        {
+            "name": "A6",
+            "type": "SUV",
+            "year": 2023,
+            "car_make": car_make_instances[2],
+        },
         {
             "name": "Sorrento",
             "type": "SUV",
